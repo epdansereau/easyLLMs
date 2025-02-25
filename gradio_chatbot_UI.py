e85e8059a6715c48ffeda4361c51908c14f53340
Etienne Pierard Dansereau <epierard@live.ca>
2025-02-25 11:32:44 -0500

    start and stop button

--- gradio_chatbot_UI.py
+++ gradio_chatbot_UI.py
@@ -5,9 +5,10 @@ class ChatInterfaceCustom(gr.Blocks):
     def __init__(self, fn):
         super().__init__()
         self.fn = fn
+        self.is_running = False
         with self:
             chatbot = gr.Chatbot(type="messages",editable=True, height="75vh")
-            msg = gr.Textbox(show_label=False, placeholder="Type a message...", container=False, submit_btn=True, stop_btn=True)
+            msg = gr.Textbox(show_label=False, placeholder="Type a message...", container=False, submit_btn=True, stop_btn=False)
             default_system = '''You are an helpful AI agent. Use the tools at your disposal to assist the user in their queries as needed. Some replies don't require any tools, only conversation. Some replies require more than one tool. Some require you to use a tool and wait for the result before continuing your answer.'''
             with gr.Accordion("System prompt", open=False):
                 system = gr.Textbox(value=default_system, show_label=False, placeholder="Enter a system prompt or leave empty for no system prompt...", container=False)
@@ -17,11 +18,30 @@ class ChatInterfaceCustom(gr.Blocks):
 
             def bot(history, system):
                 for chunk in fn(history, system):
-                    yield history + chunk
+                    if self.is_running:
+                        yield history + chunk
+                    else:
+                        break
 
-            # Capture the event chain
+            def set_running_state(msg):
+                # When a run starts, hide the submit button and show the stop button.
+                self.is_running = True
+                return gr.Textbox(submit_btn=False, stop_btn=True)
+
+            def set_idle_state(msg):
+                # When a run ends, show the submit button and hide the stop button.
+                self.is_running = False
+                return gr.Textbox(submit_btn=True, stop_btn=False)
+
             submit_event = msg.submit(
-                user, [msg, chatbot], [msg, chatbot], queue=False
+                user, [msg, chatbot], [msg, chatbot],
+            ).then(
+                set_running_state, msg, msg,
             ).then(
                 bot, [chatbot, system], chatbot
+            ).then(
+                set_idle_state, msg, msg,
+            )
+            msg.stop(
+                set_idle_state, msg, msg,
             )
